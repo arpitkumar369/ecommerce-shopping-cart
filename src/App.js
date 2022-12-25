156cb2a3fcdcd38be909f204b41c2a2390b50037
Arpit Kumar <81521655+arpitkumar369@users.noreply.github.com>
2022-12-25 15:11:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import React, { Component } from "react";
-import { Provider } from "react-redux";
+import React from 'react';
+import './App.css';
+
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Basket from "./components/Basket";
-import store from "./store";
-import "./App.css";
-import Copyright from "./components/Copyright";
 
-class App extends Component {
-    render() {
-        return (
-            <Provider store={store}>
-                <div className="container">
-                    <h1>E-commerce Shopping Cart Application</h1>
-                    <hr />
-                    <div className="row">
-                        <div className="col-md-9">
-                            <Filter />
-                            <hr />
-                            <Products />
-                        </div>
-                        <div className="col-md-3">
-                            <Basket />
-                            <Copyright />
-                        </div>
-                    </div>
-                </div>
-            </Provider>
-        );
-    }
+import { Provider } from "react-redux";
+import store from "./store";
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="container">
+          <h1>Shopping Cart Application</h1>
+          <hr />
+          <div className="row">
+            <div className="col-md-8">
+              <Filter />
+              <hr />
+              <Products />
+            </div>
+            <div className="col-md-4">
+              <Basket />
+            </div>
+          </div>
+        </div>
+        </Provider>
+    );
+  }
 }
 
 export default App;
